27eb2b3540d955a6608e706b96d582148b71de8b
alfarlost <alfarlost@yandex.ru>
2015-04-01 16:59:23 +0300

    some desperate code changes to try fix heroku

--- db/migrate/20150127082420_remove_email_column_from_customers_table.rb
+++
@@ -1,8 +0,0 @@
-class RemoveEmailColumnFromCustomersTable < ActiveRecord::Migration
-  def self.up
-    def change
-    	remove_column :customers, :email
-    	remove_column :admins, :email
-    end
-  end
-end
